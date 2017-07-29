b919ef3d73e9fb7bc7630313d30c8e7dc9e51e96
theGhostJW <theghostjw@gmail.com>
2017-07-29 10:56:14 +1000

    Added integration test

---
+++ src/test/FileUtils.integration.js
@@ -0,0 +1,12 @@
+import {chk, chkEq, chkEqJson, chkFalse} from '../lib/AssertionUtils';
+import * as _ from 'lodash';
+import jp from 'jsonpath';
+import { debug } from '../lib/SysUtils';
+
+describe('Integration', () => {
+
+  it('Hi', () => {
+
+  });
+
+});
