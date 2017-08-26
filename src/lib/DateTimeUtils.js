2e241ab70c10d0e4f8987a86b2fa2e2f3b093656
theGhostJW <theghostjw@gmail.com>
2017-08-26 15:52:03 +1000

    finished dateTimeUtils

--- src/lib/DateTimeUtils.js
+++ src/lib/DateTimeUtils.js
@@ -8,7 +8,7 @@ export const LOG_FILE_MS_SEC_FORMAT = "YYYY-MM-DD HH-mm-ss-SSS";
 
 export function testFormatter(year: number, month: number, day: number, hr24: number = 0, minute: number = 0, second: number = 0, milliSecond: number = 0, formatString: string): string {
   // moment zero bases months WTF
-  return moment({y: year, M: (month - 1), d: day, h: hr24, m: minute, s: second, ms: milliSecond}).format(formatString);
+  return time(year, month, day, hr24, minute, second, milliSecond).format(formatString);
 }
 
 export function nowAsLogFormat(): string {
@@ -22,3 +22,22 @@ export function nowFormatted(formatString: string): string {
 export function nowFileFormatted(): string {
   return moment().format(LOG_FILE_MS_SEC_FORMAT);
 }
+
+export function now(): moment$Moment {
+  return moment();
+}
+
+export function today(): moment$Moment {
+  return moment().startOf('date');
+}
+
+export function time(year: number, month: number, day: number, hr24: number = 0, minute: number = 0, second: number = 0, milliSecond: number = 0): moment$Moment {
+  // moment zero bases months WTF
+  return moment({y: year, M: (month - 1), d: day, h: hr24, m: minute, s: second, ms: milliSecond});
+}
+
+export const date = (year: number, month: number, day: number) => time(year, month, day);
+
+export const datePlus = (base: moment$Moment, daysToAdd: number) => base.add(daysToAdd, 'days');
+
+export const todayPlus = (daysToAdd: number) => datePlus(today(), daysToAdd);
--- src/test/DateTimeUtils.test.js
+++ src/test/DateTimeUtils.test.js
@@ -2,10 +2,85 @@
 
 import {test, describe} from 'mocha'
 import {debug, areEqual} from '../lib/SysUtils';
-import {  testFormatter, nowAsLogFormat, nowFileFormatted, LOG_FILE_MS_SEC_FORMAT, LOG_TO_SEC_FORMAT } from '../lib/DateTimeUtils';
+import {  testFormatter, nowAsLogFormat, nowFileFormatted,
+        LOG_FILE_MS_SEC_FORMAT, LOG_TO_SEC_FORMAT, now, today, date, time, datePlus, todayPlus } from '../lib/DateTimeUtils';
 import { chk, chkEq, chkEqJson, chkFalse, chkHasText,
         chkWithMessage
       } from '../lib/AssertionUtils';
+
+
+function jsToday() {
+  let jsDate = new Date();
+  return truncDate(jsDate);
+}
+
+function truncDate(jsDate) {
+  return new Date(jsDate.getFullYear(), jsDate.getMonth(), jsDate.getDate());
+}
+
+describe('todayPlus', () => {
+
+  it('equal today when zero', () => {
+    chkEq(jsToday() , todayPlus(0).toDate());
+  });
+
+  it('equal yestrday when -1', () => {
+    chkEq(1 * 1000 * 60 * 60 * 24, jsToday() - todayPlus(-1).toDate());
+  });
+
+});
+
+describe('datePlus', () => {
+
+  it('equal expected miliseconds from today', () => {
+    chkEq(5 * 1 * 1000 * 60 * 60 * 24,   todayPlus(5).toDate() - jsToday());
+  });
+
+});
+
+describe('time', () => {
+
+  it('should ~ equal now ', () => {
+    let momentTime = now(),
+        jsDate = new Date(),
+        expected =  time(jsDate.getFullYear(), jsDate.getMonth() + 1, jsDate.getDate(), jsDate.getHours(),
+                        jsDate.getMinutes(), jsDate.getSeconds(), jsDate.getMilliseconds());
+    chk(momentTime.toDate() - expected.toDate()  < 1000);
+  });
+
+});
+
+describe('date', () => {
+
+  it('should equal today ', () => {
+    let jsDate = jsToday();
+    // jsdate getMonth returns 0..11
+    chkEq(jsToday(), date(jsDate.getFullYear(), jsDate.getMonth() + 1, jsDate.getDate()).toDate());
+  });
+
+});
+
+describe('today', () => {
+
+  it('midnight ', () => {
+    chkEq(jsToday(), today().toDate());
+  });
+
+});
+
+
+describe('now', () => {
+
+  it('just check it does not blow up - nowAsLogFormat', () => {
+    let actual = now(),
+        jsDate = Date.now();
+
+    debug(actual.toDate());
+    chk(jsDate - actual.toDate() < 1000);
+  });
+
+});
+
 
 describe('formatters', () => {
 
