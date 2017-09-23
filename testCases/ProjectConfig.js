f6f6b0adbf9f0449f0cdc1c8a2d7b0d3ad912780
theGhostJW <theghostjw@gmail.com>
2017-09-23 15:05:16 +1000

    interim check in before changes

--- testCases/ProjectConfig.js
+++ testCases/ProjectConfig.js
@@ -3,7 +3,9 @@
 
 import type { BaseCase, BaseItem, BaseTestConfig, BaseRunConfig, GenericValidator, RunParams } from '../src/lib/TestRunner';
 import { runTestItem,  } from '../src/lib/TestRunner';
+import { forceArray } from '../src/lib/SysUtils';
 import * as caseRunner from '../src/lib/TestRunner';
+import * as _ from 'lodash';
 
 export type Environment = "TST" | "UAT" | "PVT";
 export type Depth = "Connectivity" | "Regression" | "DeepRegression" | "Special";
@@ -26,7 +28,7 @@ export type RunConfig = {|
   name: string,
   country: Country,
   environment: Environment,
-  testCases: Array<number|string> | number | string,
+  testCases: Array<number|string> | number | string | null,
   depth: Depth
 |}
 
@@ -64,6 +66,29 @@ export type Validator<V, I: BaseItem> = GenericValidator<V, I, RunConfig>
 export type Validators<V, I: BaseItem> = Validator<V, I> | Array<Validator<V, I>>
 
 export const register = <I: BaseItem, S, V>(testCase: TestCase<I, S, V>): void => caseRunner.register(testCase);
+
+function setTestConfigDefaults(partialTestConfig: TestConfig): FullTestConfig {
+  let defaultProps = {
+    countries: 'Australia',
+    environments: 'TST',
+    depth: 'Regression'
+  };
+  let result = _.defaults(partialTestConfig, defaultProps);
+  result.environments = forceArray(result.environments);
+  result.countries = forceArray(result.countries);
+  return result;
+}
+
+function setRunConfigDefaults(partialRunConfig: PartialRunConfig): RunConfig {
+  let defaultprops =  {
+    country: 'Australia',
+    environment: 'TST',
+    testCases: null,
+    depth: 'Regression'
+  };
+
+  return _.defaults(partialRunConfig, defaultprops);
+}
 
 // export const DEFAULT_RUN_CONFIG: RunConfig = {
 //   name: 'Default Config',
