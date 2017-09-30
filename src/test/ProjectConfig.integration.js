2acfe62757248d71a0c6ef81a9f20894183f0ba9
theGhostJW <theghostjw@gmail.com>
2017-09-30 20:27:34 +1000

    fixed config

--- src/test/ProjectConfig.integration.js
+++ src/test/ProjectConfig.integration.js
@@ -5,20 +5,23 @@ import {it, describe} from 'mocha'
 import { run } from '../../testCases/ProjectConfig';
 import {chk, chkEq, chkEqJson, chkFalse, chkExceptionText, chkWithMessage} from '../lib/AssertionUtils';
 import  type { Environment, Country, Depth } from '../../testCases/ProjectConfig';
+import { log } from '../lib/Logging';
 import { debug } from '../lib/SysUtils';
 
 describe.only('run', () => {
 
-  const runConfig = () => { return {
-    name: 'Test Run',
+  const runConfig = {
+    name: 'Test Test Run',
     country: 'Australia',
     environment: 'TST',
     testCases: [],
     depth: 'Regression'
-  }}
+  };
 
   it('demo Test Run', () => {
-    run(runConfig)
+    log('in test');
+    log('in test 2');
+    run(runConfig);
   });
 
 });
