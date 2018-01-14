559e1dc29b6909fe3f37569cbe5a1ec25b5f6f5d
theGhostJW <theghostjw@gmail.com>
2018-01-14 11:44:42 +1100

    added test files

---
+++ src/test/WebUtils.integration.js
@@ -0,0 +1,15 @@
+// @flow
+
+import {it, describe} from 'mocha';
+import {chk, chkEq, chkEqJson, chkFalse, chkExceptionText, chkWithMessage} from '../lib/AssertionUtils';
+import { debug } from '../lib/SysUtils';
+import { toTemp, toTempString } from '../lib/FileUtils';
+import * as _ from 'lodash';
+
+describe.only('test cafe play', () => {
+
+  it('', () => {
+
+  });
+
+});
