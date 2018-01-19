3dba147af10ffb983700e475f90b3089d0c745b5
theGhostJW <theghostjw@gmail.com>
2018-01-19 18:17:13 +1100

    more playing around

--- src/test/WebUtils.ui.js
+++ src/test/WebUtils.ui.js
@@ -6,22 +6,80 @@ import { toString } from '../lib/StringUtils';
 import { toTemp, toTempString } from '../lib/FileUtils';
 import * as _ from 'lodash';
 import * as wd from 'webdriverio';
+import * as ipc from 'node-ipc';
 
 var title;
 
 function runIt() {
+  try {
     browser.url('http://webdriver.io');
     title = browser.getTitle();
-    console.log(title + ' - PID:' + toString(process.pid));
-  //  process.stdout.write('Hi there', 'utf8')
-  //  title.notThere = 1;
+    console.log(title + ' 1 - PID:' + toString(process.pid));
+
+    browser.newWindow('http://google.com')
+    browser.url('http://google.com')
+    title = browser.getTitle();
+    console.log(title + ' 2 - PID:' + toString(process.pid));
+
+  } catch (e) {
+    debug(e.toString());
+    debug(e.stacktrace);
+    throw e;
+  }
 }
 
 describe.only('test cafe play', () => {
 
   it('interact', () => {
     runIt();
-    chkEq('WebdriverIO - WebDriver bindings for Node.js', title);
+    chkEq('Google', title);
   });
 
 });
+
+
+function runClient() {
+  ipc.config.id = 'uiTest';
+  ipc.config.retry = 1000;
+  ipc.config.sync= true;
+
+  ipc.connectTo(
+      'world',
+      function(){
+          ipc.of.world.on(
+              'connect',
+              function(){
+                  ipc.log('## started ##', ipc.config.delay);
+
+                  //queue up a bunch of requests to be sent synchronously
+                  for(var i=0; i<10; i++){
+                      ipc.of.world.emit(
+                          'next-Msg',
+                          {
+                              id      : ipc.config.id,
+                              message : 'Next Please'
+                          }
+                      );
+                  }
+              }
+          );
+
+          ipc.of.world.on(
+              'disconnect',
+              function(){
+                  ipc.log('disconnected from world');
+              }
+          );
+
+          ipc.of.uiInt.on(
+              'app.message',
+              function(data){
+                  ipc.log('got a message from world : ', data);
+              }
+          );
+
+          console.log(ipc.of.world.destroy);
+      }
+  );
+
+}
