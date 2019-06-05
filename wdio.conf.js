8e4723e29f49f1e7e6a5d626826cc956914e5d30
theGhostJW <theghostjw@gmail.com>
2019-06-05 10:15:00 +1000

    wdio config changed file path to relative

--- wdio.conf.js
+++ wdio.conf.js
@@ -19,7 +19,7 @@ exports.config = {
   // directory is where your package.json resides, so `wdio` will be called from there.
   //
   specs: [
-    'C:\\ZenithFlow\\temp\\WebInteractor.ts'
+    './temp/WebInteractor.ts'
   ],
   // Patterns to exclude.
   exclude: [
