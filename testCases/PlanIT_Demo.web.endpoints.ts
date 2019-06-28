e18af0e8296d75bf94f7df1823424f6b0ea61fb7
theGhostJW <theghostjw@gmail.com>
2019-06-28 15:32:49 +1000

    more gardening

--- testCases/PlanIT_Demo.web.endpoints.ts
+++ testCases/PlanIT_Demo.web.endpoints.ts
@@ -3,8 +3,6 @@ import * as _ from 'lodash';
 import { allItems } from '../src/lib/TestRunner';
 import { testCase } from './PlanIT_Demo.web';
 import { testCaseEndPoint, run } from './ProjectConfig';
-import { toTemp } from '../src/lib/FileUtils';
-import { wdDebug } from '../src/lib/WebUtils';
 
 describe('endPoint', () => {
 
